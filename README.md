e7aef25be589b63370203e02a78dd519d7f5165d
Fernando A. Damião <me@fadamiao.com>
2012-10-24 18:39:39 -0200

    Primeira inclusão de arquivos no putty.bat

---
+++ README.md
@@ -0,0 +1,27 @@
+putty.bat
+=========
+
+PuTTY Backup and Restorer.
+
+## Specs
+* Feito no SO : Windows 7 SP1
+* Testado no SO : Windows 7 SP1
+* Linguagem Utilizada : Batch Script
+
+## Motivation
+Essa ferramenta foi feita para criar e restaurar o backup das configurações do PuTTY.
+
+## How To
+Basta acessar o diretório onde está o putty.bat e chama-lo: 
+
+```
+putty.bat
+==== PuTTY Backup and Restorer ====
+1 - Fazer Backup
+2 - Restaurar Backup
+0 - Sair
+Digite o numero da opcao: 
+```
+
+Ou abri-lo pela interface gráfica.
+
---
+++ putty.bat
@@ -0,0 +1,49 @@
+@echo off
+title PuTTY Backup and Restorer
+goto :menu
+
+
+:menu
+echo ==== PuTTY Backup and Restorer ====
+
+echo 1 - Fazer Backup
+echo 2 - Restaurar Backup
+echo 0 - Sair
+set /p menu=Digite o numero da opcao: 
+if "%menu%"=="0" (goto :exit)
+if "%menu%"=="1" (goto :dobackup)
+if "%menu%"=="2" (goto :restorebackup)
+goto :menu
+
+
+:dobackup
+
+set /p file=Digite o nome do arquivo: 
+regedit /e %file% HKEY_CURRENT_USER\Software\SimonTatham
+echo.
+goto :menu
+
+
+
+:restorebackup
+set /p file=Digite o nome do arquivo: 
+
+If Exist %file% (goto :restore)
+If Not Exist %file% (goto :restorefail)
+
+
+
+:restore
+
+regedit /s %file%
+
+goto :menu
+
+:restorefail
+echo Arquivo nao encontrado!
+echo.
+goto :menu
+
+
+:exit
+exit
